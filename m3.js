7730c3dbba0765bc984cac4846128e35eb335894
r-exs <harryledevk@gmail.com>
2025-11-26 13:43:37 +0700

    Update and rename m2.js to m3.js

--- m3.js
+++ m3.js
@@ -2424,7 +2424,7 @@ ${Dinosaur.config.textCustomizeCSS}
         generateAvatarName: (fullName) => {
             const initials = fullName
                 ?.split(' ')
-                ?.map(word => word[0].toUpperCase())
+                ?.map(word => word[0]?.toUpperCase())
                 ?.slice(0, 2)
                 ?.join('');
             return initials;
